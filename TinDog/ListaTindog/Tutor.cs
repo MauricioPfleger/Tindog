dd347734408a00fa39594a4deb2b4f5adbb4851c
mauricio <mauriciopfleger7@gmail.com>
2024-03-31 17:13:37 -0300

    New

---
+++ TinDog/ListaTindog/Tutor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTindog
+{
+    internal class Tutor
+    {
+        public string? Nome {  get; set; }
+
+        public List<Pet> Pets;
+        public Tutor() { 
+            Pets = new List<Pet>();
+        }
+    }
+}
