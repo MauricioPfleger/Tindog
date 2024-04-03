16f500ea5e5aec9b7cdba6df57574a479cf789b2
mauricio <mauriciopfleger7@gmail.com>
2024-04-03 20:38:56 -0300

    Adicionando a propriedade Endereco

--- TinDog/ListaTindog/Tutor.cs
+++ TinDog/ListaTindog/Tutor.cs
@@ -12,6 +12,8 @@ namespace ListaTindog
 
         public int Idade { get; set; }
 
+        public string? Endereco { get; set; }
+
         public List<Pet> Pets;
         public Tutor() { 
             Pets = new List<Pet>();
